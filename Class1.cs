b74dda4c19a073b389cbafbf2c80e1701a749788
Peret_pu85@itstepru.onmicrosoft.com <Peret_pu85@itstepru.onmicrosoft.com>
2023-07-26 20:52:29 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCatalog
+{
+    internal class Class1
+    {
+    }
+    public class FileItem
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+
+    public class DirectoryItem
+    {
+        public string Name { get; set; }
+        public ObservableCollection<FileItem> Files { get; set; }
+
+        public DirectoryItem(string path)
+        {
+            Name = System.IO.Path.GetFileName(path);
+            Files = new ObservableCollection<FileItem>();
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                var fileInfo = new FileInfo(file);
+
+                Files.Add(new FileItem()
+                {
+                    Name = fileInfo.Name,
+                    Size = fileInfo.Length,
+                    DateModified = fileInfo.LastWriteTime
+                });
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Windows.Forms;
+
+namespace WpfCatalog
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+
+        private void LoadDirectory(string path)
+        {
+            var directoryItem = new DirectoryItem(path);
+            listik.ItemsSource = directoryItem.Files;
+            expander.Header = directoryItem.Name;
+            expander.Content = listik;
+        }
+
+        private void clk(object sender, RoutedEventArgs e)
+        {
+            string pp = @"E:\Course";
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            pp = dialog.SelectedPath;
+            //if (dialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    pp = dialog.SelectedPath;
+            //   
+            //}
+             //pp = @"E:\Course";//Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\");
+            LoadDirectory(pp);
+        }
+    }
+}
